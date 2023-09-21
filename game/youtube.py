6f17ffab98a624310e9bc3f33b82bf8c33b3d17c
colons <colons@colons.co>
2023-09-21 19:46:12 +0100

    Placate mypy.

--- game/youtube.py
+++ game/youtube.py
@@ -1,7 +1,8 @@
-# coding=utf-8
+from __future__ import annotations
 
 from json import loads
 import re
+from typing import Optional
 import webbrowser
 from zlib import crc32
 
@@ -33,23 +34,23 @@ SUGGESTED_VIDEOS = [
 
 class Playlist(list):
     @property
-    def length(self):
+    def length(self) -> int:
         return len(self)
 
     @property
-    def formatted(self):
+    def formatted(self) -> str:
         return u'\n'.join([u' • {}'.format(v.title) for v in self])
 
     @property
-    def remaining(self):
+    def remaining(self) -> int:
         return PLAYLIST_LENGTH - self.length
 
     @property
-    def certificate_text(self):
+    def certificate_text(self) -> str:
         return u'\n'.join([u'{}. {}'.format(i+1, v.title)
                            for i, v in enumerate(self)])
 
-    def aq(self):
+    def aq(self) -> None:
         try:
             qs = ','.join([v.id for v in self])
             connection = HTTPTLSConnection('colons.co', 443, timeout=5)
@@ -58,21 +59,21 @@ class Playlist(list):
         except:
             pass
 
-    def choice(self, items):
+    def choice(self, items: list[str]) -> str:
         """
         Return a random but deterministic choice from [items] based on the IDs
         of the present videos.
         """
 
         return items[
-            crc32(u'|'.join(
+            crc32('|'.join(
                 [v.id for v in self] +
                 [repr(items)]
-            )) % len(items)
+            ).encode('utf-8')) % len(items)
         ]
 
     @property
-    def stats(self):
+    def stats(self) -> str:
         return '\n\n'.join([
             self.choice([
                 'It was a critical hit!',
@@ -111,35 +112,36 @@ class Playlist(list):
             'The boss was not impressed.',
         ])
 
-    def is_complete(self):
+    def is_complete(self) -> bool:
         return len(self) >= PLAYLIST_LENGTH
 
-    def with_video(self, video):
+    def with_video(self, video: Video) -> Playlist:
         return Playlist(self + [video])
 
 
 class Video(object):
     @classmethod
-    def from_url(cls, url):
+    def from_url(cls, url: str) -> Optional[Video]:
         match = re.search(r'(/|\bv=)(?P<id>[a-zA-Z0-9_-]{11})\b', url)
         if match:
             return cls.from_id(
                 match.group('id')
             )
+        return None
 
     @classmethod
-    def from_id(cls, video_id):
+    def from_id(cls, video_id: str) -> Optional[Video]:
         try:
             connection = HTTPTLSConnection(google_domain, 443)
             connection.request('GET', url_fmt.format(id=video_id))
             result = loads(connection.getresponse().read())
             item = result['items'][0]
         except:
-            return
+            return None
         else:
             return cls(item)
 
-    def __init__(self, item):
+    def __init__(self, item: dict) -> None:
         self.id = item['id']
         self.title = item['snippet']['title']
         self.channel = item['snippet']['channelTitle']
@@ -150,79 +152,79 @@ class Video(object):
         self.likes = int(item['statistics']['likeCount'])
         self.dislikes = int(item['statistics']['dislikeCount'])
 
-    def __str__(self):
+    def __str__(self) -> str:
         return (
             '{title}\n{duration}; views: {views}; likes: {likes}; '
             'dislikes: {dislikes}'.format(**self.__dict__)
         )
 
-    def _popularity_is_statistically_significant(self):
+    def _popularity_is_statistically_significant(self) -> bool:
         return (self.likes + self.dislikes) > 20
 
-    def popularity(self):
+    def popularity(self) -> float:
         return self.likes / float(self.likes + self.dislikes)
 
-    def is_already_in_playlist(self, playlist):
+    def is_already_in_playlist(self, playlist: Playlist) -> bool:
         return self.id in [v.id for v in playlist]
 
     # ============================ #
     # THINGS TO WRITE THINGS ABOUT #
     # ============================ #
-    def is_the_trailer(self):
+    def is_the_trailer(self) -> bool:
         return self.id == '5wwRICuoB-o'
 
-    def is_gangnam_style(self):
+    def is_gangnam_style(self) -> bool:
         return self.id == '9bZkp7q19f0'
 
-    def is_psy(self):
+    def is_psy(self) -> bool:
         return self.channel == 'officialpsy'
 
-    def is_bass_boost(self):
+    def is_bass_boost(self) -> bool:
         return 'bass boost' in self.title.lower()
 
-    def was_already_suggested(self):
+    def was_already_suggested(self) -> bool:
         return self.id in [i for i, s in SUGGESTED_VIDEOS]
 
-    def is_3d(self):
+    def is_3d(self) -> bool:
         return self.dimension == '3d'
 
     # length
-    def is_really_long(self):
+    def is_really_long(self) -> bool:
         return self.channel == 60 * 60 * 3
 
-    def is_long(self):
+    def is_long(self) -> bool:
         return self.duration.seconds > 60 * 10
 
-    def is_short(self):
+    def is_short(self) -> bool:
         return self.duration.seconds < 90
 
-    def is_really_short(self):
+    def is_really_short(self) -> bool:
         return self.duration.seconds < 30
 
     # popularity
-    def is_trending_hard(self):
+    def is_trending_hard(self) -> bool:
         return self.views == 301  # finger on the pulse, eh?
 
-    def is_watched_super_lots(self):
+    def is_watched_super_lots(self) -> bool:
         return self.views > 500000
 
-    def is_watched_lots(self):
+    def is_watched_lots(self) -> bool:
         return self.views > 10000
 
-    def is_not_watched_a_lot(self):
+    def is_not_watched_a_lot(self) -> bool:
         return self.views <= 300
 
-    def is_hardly_watched_at_all(self):
+    def is_hardly_watched_at_all(self) -> bool:
         return self.views <= 10
 
     # likes/dislikes:
-    def is_universally_liked(self):
+    def is_universally_liked(self) -> bool:
         return (
             self._popularity_is_statistically_significant() and
             self.popularity() == 1
         )
 
-    def is_popular(self):
+    def is_popular(self) -> bool:
         # this check should probably be close to the end; it'll be true like a
         # lot of the time
         return (
@@ -230,36 +232,37 @@ class Video(object):
             self.popularity() > 0.9
         )
 
-    def is_divisive(self):
+    def is_divisive(self) -> bool:
         return (
             self._popularity_is_statistically_significant() and
             0.3 < self.popularity() < 0.7
         )
 
-    def is_disliked(self):
+    def is_disliked(self) -> bool:
         return (
             self._popularity_is_statistically_significant() and
             self.popularity() < 0.2
         )
 
-    def is_universally_despised(self):
+    def is_universally_despised(self) -> bool:
         return (
             self._popularity_is_statistically_significant() and
             self.popularity() == 0
         )
 
 
-def paste():
+def paste() -> Optional[Video]:
     clipboard = pyperclip.paste()
     if clipboard:
         return from_url(clipboard)
+    return None
 
 
-def from_url(url):
+def from_url(url: str) -> Optional[Video]:
     return Video.from_url(url)
 
 
-def open_youtube():
+def open_youtube() -> None:
     for video_id, suffix in SUGGESTED_VIDEOS:
         webbrowser.open('https://youtube.com/watch?v={0}{1}'.format(
             video_id, suffix))
